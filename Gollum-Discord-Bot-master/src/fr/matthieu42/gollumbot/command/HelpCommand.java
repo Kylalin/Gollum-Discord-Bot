e6b6d03096028c49605895fbabc5fd914f4d7c1e
Kylalin <58925147+Kylalin@users.noreply.github.com>
2024-01-19 20:12:33 -0600

    Add files via upload

---
+++ Gollum-Discord-Bot-master/src/fr/matthieu42/gollumbot/command/HelpCommand.java
@@ -0,0 +1,32 @@
+package fr.matthieu42.gollumbot.command;
+
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.awt.*;
+
+public class HelpCommand extends Command{
+
+
+    private Commands commands;
+
+    public HelpCommand(Commands commands) {
+        super("!aide", "Affiche l'aide du bot");
+        this.commands = commands;
+    }
+
+    @Override
+    public void execute(MessageReceivedEvent event) {
+        TextChannel currentChannel = event.getTextChannel();
+        User botUser = event.getJDA().getSelfUser();
+        EmbedBuilder help = new EmbedBuilder();
+        help.setAuthor(botUser.getName(),"http://matthieu42.fr",botUser.getAvatarUrl());
+        help.setTitle("Les différentes commandes");
+        for(Command c : commands.getCommandList()){
+            help.addField("```" + c.getName() + "```",c.getDescription(),false);
+        }
+        help.setColor(Color.BLUE);
+        currentChannel.sendMessage(help.build()).complete();
+    }
+}
